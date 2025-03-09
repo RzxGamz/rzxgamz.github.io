473337547748f5a315b1550ab364c8d9e6e0244e
RZXGAMZ <97107434+RzxGamz@users.noreply.github.com>
2025-03-10 00:11:22 +0700

    Update rzxgamz.java

--- assets/vendor/php-email-form/rzxgamz.java
+++ assets/vendor/php-email-form/rzxgamz.java
@@ -1,1 +1,27 @@
+public class SeniTeks {
 
+    public static void main(String[] args) {
+        String teks = "WEBOT";
+        int panjang = teks.length();
+
+        for (int i = 0; i < panjang; i++) {
+            for (int j = 0; j < panjang - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(teks.charAt(j) + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = panjang - 2; i >= 0; i--) {
+            for (int j = 0; j < panjang - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(teks.charAt(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
