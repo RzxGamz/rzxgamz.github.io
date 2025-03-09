167b01c6cf688850ca28c1f335b6fe6289008667
RZXGAMZ <97107434+RzxGamz@users.noreply.github.com>
2025-03-10 00:10:07 +0700

    Update rzxgamz.go

--- assets/vendor/imagesloaded/rzxgamz.go
+++ assets/vendor/imagesloaded/rzxgamz.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+        "fmt"
+        "math/rand"
+        "time"
+)
+
+func main() {
+        rand.Seed(time.Now().UnixNano())
+        angkaRahasia := rand.Intn(100)
+        percobaan := 0
+
+        fmt.Println("Selamat datang di Permainan Tebak Angka!")
+        fmt.Println("Saya telah memilih angka rahasia antara 0 dan 99.")
+        fmt.Println("Bisakah kamu menebaknya?")
+
+        for {
+                percobaan++
+                var tebakan int
+                fmt.Print("Masukkan tebakanmu: ")
+                _, err := fmt.Scanln(&tebakan)
+                if err != nil {
+                        fmt.Println("Masukan tidak valid. Silakan masukkan angka.")
+                        continue
+                }
+
+                if tebakan < angkaRahasia {
+                        fmt.Println("Terlalu rendah!")
+                } else if tebakan > angkaRahasia {
+                        fmt.Println("Terlalu tinggi!")
+                } else {
+                        fmt.Printf("Selamat! Kamu berhasil menebak angka rahasia dalam %d percobaan!\n", percobaan)
+                        break
+                }
+        }
+}
